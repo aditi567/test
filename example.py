ba4c39e633294760ad7c3018883a86ee71687af0
Aditi Davange <101041462+Aditi5670@users.noreply.github.com>
2024-03-17 15:12:19 +0530

    dev b commited

--- example.py
+++ example.py
@@ -1,7 +1,6 @@
-# example.py
-
+# Developer B's changes
 def greet():
-    return "Hello, World!"
+    return "Hello, GitHub!"  # Developer B's change
 
 if __name__ == "__main__":
     print(greet())
